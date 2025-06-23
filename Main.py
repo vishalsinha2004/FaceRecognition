b0b4ab9460595892b098c13e108a0a99c3131daf
vishalsinha2004 <vishalsinha6567@gmail.com>
2025-06-23 11:40:11 +0530

    Fix video data flipping in face detection loop

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ face_cap = cv2.CascadeClassifier("C:/Users/VISHAL/AppData/Local/Programs/Python/
 video_cap = cv2.VideoCapture(0)
 while True:
     ret , video_data = video_cap.read()
+    video_data=cv2.flip(video_data, 1)
     col = cv2.cvtColor(video_data, cv2.COLOR_BGR2GRAY)
     faces = face_cap.detectMultiScale(
         col,
@@ -10,6 +11,7 @@ while True:
         minNeighbors=5,
         minSize=(30, 30),
         flags=cv2.CASCADE_SCALE_IMAGE
+        
     )
     for (x, y, w, h) in faces:
         cv2.rectangle(video_data, (x, y), (x + w, y + h), (0, 255, 0), 2)
